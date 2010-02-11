0fdecebe54b358abf82e45bd30c58665a5a4ed33
Yeison Rodriguez <yeison@yul.(none)>
2010-02-11 07:15:04 -0500

    Having trouble with intensity accumulation 2
    The images indicate that all the values are resulting zero.

--- ans1.py
+++ ans1.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python
-from opencv import highgui
-from opencv import cv
-from math import sin, cos
-from math import pi
-from math import sqrt
+from opencv import highgui, cv
+from math import sin, cos, pi, sqrt
 import sys, getopt
 
 
 image = highgui.cvLoadImage("circle.jpg", highgui.CV_LOAD_IMAGE_GRAYSCALE)
 
+#Determines if the angle is diagonal from the origin.
+def checkAngle(theta):
+    if((theta*2)/pi == int((theta*2)/pi)):
+        return 1
+    else:
+        return sqrt(2) 
 
-#myX and myY represent the calling pixel's location.
+
 def getPixelValues(x, y, theta, scale):
 #Pixel value array
     pxArray = []
     sum = 0
-    if((theta*2)/pi == int((theta*2)/pi)):
-        d = 1
-    else:
-        d = sqrt(2) 
-    
+    d = checkAngle(theta)
     for i in range(scale):
         #print "x: %s" %  int(x + i*cos(theta))
         pxArray.append(image[int(x + i*d*cos(theta)), int(y + i*d*sin(theta))])
@@ -28,20 +27,34 @@ def getPixelValues(x, y, theta, scale):
         sum += pxArray[i]
     return (sum/scale)
 
+#Shorten the function name
+iA1 = intensityAccum1 = getPixelValues
 
-def solveHWProblem(theta, scale):
-    fileName = "theta%s_s%s.jpg" % (theta, scale)
+def intensityAccum2(x, y, theta, s):
+    d = checkAngle(theta)
+    if(d == 1):
+        return 1/4*(iA1(x, y, theta, s) + iA1(x+d*cos(theta), y+d*sin(theta), theta, s) + iA1(x-d*sin(theta), y+d*cos(theta), theta, s) + iA1(x+d*sin(theta), y-d*sin(theta), theta, s))
+    else:
+        return 1/4*(iA1(x, y, theta, s) + iA1(x+d*cos(theta), y+d*sin(theta), theta, s) + iA1(x, y+d*sin(theta), theta, s) + iA1(x+d*cos(theta), y, theta, s))
+
+iA2 = intensityAccum2
+
+def solveHWProblem(theta, scale, function):
+    fileName = "theta%s_s%s_%s.jpg" % (theta, scale, function)
     size = cv.cvSize(image.width - 2*scale , image.height - 2*scale)
     theta_image = cv.cvCreateImage(size, cv.IPL_DEPTH_8U, 1)
     #range(s, value): stay s pixels away from all boundaries.
     #print range(scale, image.height -scale)
     for y in range(scale, image.height - scale):
         for x in range(scale, image.width - scale):
-            theta_image[x-scale, y-scale] =  getPixelValues(x, y, theta, scale)
+            theta_image[x-scale, y-scale] =  function(x, y, theta, scale)
             #if(image[x, y] > 0 and image[x, y] < 255):
 
     
     highgui.cvSaveImage(fileName, theta_image)
 
-for i in range(8):
-    solveHWProblem((i*pi)/4, 5)
+#for i in range(8):
+#    solveHWProblem((i*pi)/4, 5)
+
+
+solveHWProblem(0, 3, iA2)
