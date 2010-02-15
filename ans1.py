c5a595fb23b9372723bc90be3c596c245a819c20
Yeison Rodriguez <yeison@yul.(none)>
2010-02-14 19:26:14 -0500

    The assignment is nearly finished.  Just have to look at the last question.
    It took a little bit of problems solving to work with opencv.  It wouldn't let me save 8bit-signed-integers.  So finally I figured out that the image buffer was formatting my values automatically when I saved them in there.  I decided to save them in an array, then scale them myself.  Now the intensity differential can be seen, since the black background pixels have been scaled to gray.

--- ans1.py
+++ ans1.py
@@ -6,7 +6,7 @@ import sys, getopt
 
 image = highgui.cvLoadImage("circle.jpg", highgui.CV_LOAD_IMAGE_GRAYSCALE)
 
-#Determines if the angle is diagonal from the origin.
+#Determines if the angle is diagonal with respect to the x or y axes.
 def checkAngle(theta):
     if((theta*2)/pi == int((theta*2)/pi)):
         return 1
@@ -26,35 +26,53 @@ def getPixelValues(x, y, theta, scale):
     for i in range(scale):
         sum += pxArray[i]
     return (sum/scale)
-
 #Shorten the function name
 iA1 = intensityAccum1 = getPixelValues
 
+
 def intensityAccum2(x, y, theta, s):
     d = checkAngle(theta)
-    if(d == 1):
-        return 1/4*(iA1(x, y, theta, s) + iA1(x+d*cos(theta), y+d*sin(theta), theta, s) + iA1(x-d*sin(theta), y+d*cos(theta), theta, s) + iA1(x+d*sin(theta), y-d*sin(theta), theta, s))
-    else:
-        return 1/4*(iA1(x, y, theta, s) + iA1(x+d*cos(theta), y+d*sin(theta), theta, s) + iA1(x, y+d*sin(theta), theta, s) + iA1(x+d*cos(theta), y, theta, s))
-
+    if(d == 1): #If theta is along a straight line.
+        term1 = iA1(x, y, theta, s)
+        term2 = iA1(x+d*cos(theta), y+d*sin(theta), theta, s)
+        term3 = iA1(x-d*sin(theta), y+d*cos(theta), theta, s)
+        term4 = iA1(x+d*sin(theta), y-d*cos(theta), theta, s)
+        ia2 = (term1 + term2 + term3 + term4)/4
+        return ia2
+    else:       #If theta is along a diagonal.
+        term1 = iA1(x, y, theta, s)
+        term2 = iA1(x+d*cos(theta), y+d*sin(theta), theta, s)
+        term3 = iA1(x, y+d*sin(theta), theta, s)
+        term4 = iA1(x+d*cos(theta), y, theta, s)
+        ia2 = (term1 + term2 + term3 + term4)/4
+        return ia2
+#Shorten function name
 iA2 = intensityAccum2
 
+#Derivative of iA2 along angle theta.
+def dofIA2(x, y, theta, s):
+    d = checkAngle(theta)
+    return iA2(x, y, theta, s) - iA2(x-d*cos(theta), y-d*sin(theta), pi + theta, s)
+
+
+thetaPxArray = []
 def solveHWProblem(theta, scale, function):
-    fileName = "theta%s_s%s_%s.jpg" % (theta, scale, function)
+    fileName = "newimages/theta%spi_s%s_%s.jpg" % (theta/pi, scale, function.__name__)
     size = cv.cvSize(image.width - 2*scale , image.height - 2*scale)
     theta_image = cv.cvCreateImage(size, cv.IPL_DEPTH_8U, 1)
     #range(s, value): stay s pixels away from all boundaries.
     #print range(scale, image.height -scale)
     for y in range(scale, image.height - scale):
         for x in range(scale, image.width - scale):
-            theta_image[x-scale, y-scale] =  function(x, y, theta, scale)
+            if(function.__name__ == "dofIA2") :
+                theta_image[x-scale, y-scale] = function(x, y, theta, scale)/2 + 128
+            else:
+                theta_image[x-scale, y-scale] = function(x, y, theta, scale)
             #if(image[x, y] > 0 and image[x, y] < 255):
-
-    
     highgui.cvSaveImage(fileName, theta_image)
 
 #for i in range(8):
 #    solveHWProblem((i*pi)/4, 5)
 
 
-solveHWProblem(0, 3, iA2)
+solveHWProblem(pi/4, 5, dofIA2)
