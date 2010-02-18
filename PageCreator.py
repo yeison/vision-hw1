670d0d28d11f32d5b1ad69f57f97e0f655c0f32d
Yeison Rodriguez <yeirodr@gmail.com>
2010-02-17 23:52:24 -0500

    Nearly finished with web publishing code.

--- PageCreator.py
+++ PageCreator.py
@@ -2,7 +2,30 @@ from BeautifulSoup import BeautifulSoup, Tag, NavigableString
 import os
 
 class HTMLPage:
-    
+    def insertImage(self, original, angle, scale, function):
+        soup = self.soup
+        table = Tag(soup, "table")
+        tr = Tag(soup, "tr")
+        td = Tag(soup, "td")
+        img = Tag(soup, "img")
+        
+        if not soup.find(id=original): 
+            soup.body.insert(0, table)
+            table['id'] = original
+
+        name_angle = "%s%s" % (original, angle) 
+        if not soup.find(id=name_angle):
+            soup.find(id=original).insert(0, tr)
+            tr['id'] = name_angle
+            
+        angle_scale_function = "%s%s%s" % (name_angle, scale, function)
+        if not soup.find(id=angle_scale):
+            soup.find(id=name_angle).insert(0, td)
+            td['id'] = angle_scale_function
+
+        td.insert(0, img)
+        
+
 
     def newHtml(self, pageName):
         file = open(pageName, 'w')
@@ -31,8 +54,17 @@ class HTMLPage:
            self.newHtml(pageName)
         self.file = open(pageName, 'rw')
         self.html = self.file.read()
-        soup = BeautifulSoup(self.html)
-        print soup('td')[2]
+        self.soup = BeautifulSoup(self.html)
+        img = Tag(self.soup, "img")
+        img['src'] = "http//word"
+        self.soup('td')[2].insert(0, img)
+        img = Tag(self.soup, "img")
+        self.soup('td')[2].insert(0, img)
+        this = "str1"
+        that = "str2"
+        print "%s%s" % (this, that)
+        
+
             
 
 page = HTMLPage()
